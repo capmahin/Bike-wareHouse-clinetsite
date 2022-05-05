c5a78b8cb57e6890738b1c93ea3c41d926c62200
capmahin <mahinulhuque1996@gmail.com>
2022-05-05 14:30:36 +0600

    add a Experts component and map it

---
+++ src/Pages/Home/Experts/Experts.js
@@ -0,0 +1,32 @@
+import React from "react";
+import expert1 from "../../../images/Experts/expert1.jpg";
+import expert2 from "../../../images/Experts/expert2.jpg";
+import expert3 from "../../../images/Experts/expert3.jpg";
+import expert4 from "../../../images/Experts/expert4.jpg";
+import expert5 from "../../../images/Experts/expert5.jpg";
+import expert6 from "../../../images/Experts/expert6.jpg";
+import Expert from "../Expert/Expert";
+
+const experts = [
+  { id: 1, name: "john corner", img: expert1 },
+  { id: 2, name: "john corner", img: expert2 },
+  { id: 3, name: "john corner", img: expert3 },
+  { id: 4, name: "john corner", img: expert4 },
+  { id: 5, name: "john corner", img: expert5 },
+  { id: 6, name: "john corner", img: expert6 },
+];
+
+const Experts = () => {
+  return (
+    <div className="container">
+      <h2 className="text-dark text-center">Our Experts</h2>
+      <div className="row">
+        {experts.map((expert) => (
+          <Expert key={expert.id} expert={expert}></Expert>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Experts;

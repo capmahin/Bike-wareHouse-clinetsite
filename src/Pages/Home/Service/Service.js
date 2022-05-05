76e171e04db2de326e638d95379b42b056d04715
capmahin <mahinulhuque1996@gmail.com>
2022-05-05 12:53:09 +0600

    resopnsive it for mobile and tablate

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,11 +1,14 @@
 import React from "react";
+import "./Service.css";
 
 const Service = ({ service }) => {
   const { name, img, description, price } = service;
   return (
-    <div>
+    <div className="service">
       <img src={img} alt="" />
-      <h1>Service:{name}</h1>
+      <h2>Service:{name}</h2>
+      <p>Price:{price}</p>
+      <button>Book:{name}</button>
     </div>
   );
 };

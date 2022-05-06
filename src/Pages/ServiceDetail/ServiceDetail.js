74e45170d58b4911d5bed8adec73a6dc332ca4bb
capmahin <mahinulhuque1996@gmail.com>
2022-05-07 03:19:47 +0600

    add signOut

--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const ServiceDetail = () => {
   const { serviceId } = useParams();
   return (
     <div>
       <h2>Service Details:{serviceId}</h2>
+      <div className="text-center">
+        <Link to="/checkout">
+          {" "}
+          <button className="btn btn-dark">Proceed CheckOut </button>
+        </Link>
+      </div>
     </div>
   );
 };

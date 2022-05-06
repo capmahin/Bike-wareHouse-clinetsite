a417fb0780609cb8998e1bd15c9f1bc411a0eb74
capmahin <mahinulhuque1996@gmail.com>
2022-05-07 03:02:08 +0600

    add requireAuth

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,16 +1,35 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
+
 import "./Register.css";
 
 const Register = () => {
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
+
   const navigate = useNavigate();
   const navigateLogin = () => {
     navigate("/login");
   };
+
+  if (user) {
+    navigate("/home");
+  }
+
+  const handleRegister = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+
+    createUserWithEmailAndPassword(email, password);
+  };
   return (
     <div className="register-from">
       <h1 style={{ textAlign: "center" }}>Please Register</h1>
-      <form>
+      <form onSubmit={handleRegister}>
         <input type="text" name="name" id="" placeholder="Your Name" />
         <input type="email" name="email" id="" placeholder="Your Email" />
         <input

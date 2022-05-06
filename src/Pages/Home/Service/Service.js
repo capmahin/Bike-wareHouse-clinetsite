d3b6749b7f83c19f4ba666432986068058232303
capmahin <mahinulhuque1996@gmail.com>
2022-05-06 19:15:12 +0600

    add login page and register

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,14 +1,24 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./Service.css";
 
 const Service = ({ service }) => {
-  const { name, img, description, price } = service;
+  const { id, name, img, description, price } = service;
+  const navigate = useNavigate();
+  const navigateToServiceDetail = (id) => {
+    navigate(`/service/${id}`);
+  };
   return (
     <div className="service">
       <img className="w-100" src={img} alt="" />
       <h2>Service:{name}</h2>
       <p>Price:{price}</p>
-      <button className="btn btn-dark">Book:{name}</button>
+      <button
+        onClick={() => navigateToServiceDetail(id)}
+        className="btn btn-dark"
+      >
+        Book:{name}
+      </button>
     </div>
   );
 };
---
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+import crush from "../../../images/Crash.jpg";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h2 className="text-dark text-center">bike Was Crush</h2>
+      <img src={crush} alt="" />
+    </div>
+  );
+};
+
+export default NotFound;
